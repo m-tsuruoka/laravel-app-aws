822df041be96761c81741384bfcb941076cf7dbb
m.tsuruoka <m.tsuruoka@core-tech.jp>
2026-05-28 13:23:17 +0900

    CI/CD

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+  public function test_the_application_returns_a_successful_response(): void
+{
+    $this->fail('意図的な失敗：Branch Protection の動作確認用');
+}
 }
